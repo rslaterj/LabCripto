494ea3f33e0b5f416688b6cf396a273250dd430b
Raúl Slater <88496490+rslaterj@users.noreply.github.com>
2025-06-21 15:30:27 -0400

    Delete LabCripto4 directory

--- LabCripto4/main.py
+++
@@ -1,6 +0,0 @@
-import os
-for n,i,p in [("cont_c1","laboratorio_c1","2221"),
-              ("cont_c2","laboratorio_c2","2222"),
-              ("cont_c3","laboratorio_c3","2223"),
-              ("cont_c4","laboratorio_c4","2224")]:
-    os.system(f"docker run -d --name {n} -p {p}:22 {i}")
